530387294e5ed26d160c07d15abbbb9e93cfb9b8
Ken <knetngyn@gmail.com>
2022-07-18 15:51:22 -0500

    Add and style buttons, add basic math functions

---
+++ script.js
@@ -0,0 +1,12 @@
+function add(num, ber) {
+  return num + ber;
+}
+function multiply(num, ber) {
+  return num * ber;
+}
+function subtract(num, ber) {
+  return num - ber;
+}
+function divide(num, ber) {
+  return num / ber;
+}
