52c9c933031a7bbd5df2cae9439eda4833527cfc
Ken <knetngyn@gmail.com>
2022-07-18 16:14:11 -0500

    Create function to operate equations

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function subtract(num, ber) {
 function divide(num, ber) {
   return num / ber;
 }
+function operate(operator, num, ber) {
+  switch (operator) {
+    case "add":
+      return add(num, ber);
+    case "multiply":
+      return multiply(num, ber);
+    case "subtract":
+      return subtract(num, ber);
+    case "divide":
+      return divide(num, ber);
+    default:
+      return "mf uuuuhhhh";
+  }
+}
